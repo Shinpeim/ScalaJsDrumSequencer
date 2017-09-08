cdbd028b20f646fa1f3b66e5bd3203eca31a8f20
shinpei maruyama <shinpeim@gmail.com>
2017-09-08 16:49:52 +0900

    Safari対応

--- web/src/js/index.js
+++ web/src/js/index.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import Application from './components/Application.vue'
 
+if (window.webkitAudioContext) {
+    window.AudioContext = window.webkitAudioContext;
+}
+
 new Vue({
-      el: '#app',
-      components: { Application }
+    el: '#app',
+    components: { Application }
 });
